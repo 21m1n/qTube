86eb919cc8be404ae5a37200a1e779eab265a4f9
valeriewoon <valeriewoon.2020@scis.smu.edu.sg>
2024-05-21 18:04:01 +0800

    push

--- gradioFrontend/app.py
+++ gradioFrontend/app.py
@@ -1,14 +1,12 @@
 import gradio as gr
 
-def greet(name, is_morning, temperature):
-    salutation = "Good morning" if is_morning else "Good evening"
-    greeting = f"{salutation} {name}. It is {temperature} degrees today"
-    celsius = (temperature - 32) * 5 / 9
-    return greeting, round(celsius, 2)
+def greet(url, api):
+    return "Hello, this is the url: " + url + " api : " + api + "!"
 
 demo = gr.Interface(
     fn=greet,
-    inputs=["text", "checkbox", gr.Slider(0, 100)],
-    outputs=["text", "number"],
+    inputs=["text", "text"],
+    outputs=[gr.Textbox(label="greeting", lines=4)],
 )
+
 demo.launch()
